1c814f25a1b493b9e394b84fe99c7ad4728d6ba6
Sukharev Mikhail <elmihailol@gmail.com>
2018-10-10 16:40:25 +0700

    Added README

--- helpers.py
+++ helpers.py
@@ -2,6 +2,12 @@ import numpy
 
 
 def add_noise(x, noise_factor=0.1):
+    """
+    Добавляет шум в матрицу
+    :param x: оригинальная матрица (картинка)
+    :param noise_factor: фактор шума
+    :return: картинка (матрица) с шумом
+    """
     x = x + numpy.random.randn(*x.shape) * noise_factor
     x = x.clip(0., 5.)
     return x
